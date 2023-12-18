b5940e4668e51109ed205cb776421d71d5c6ff21
sharif <sharif@ajab.sharifulislam.com>
2023-12-18 12:08:43 +0300

    understanding styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
-function App() {
-  return <h1>vite </h1>;
-}
+import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <StyledApp>
+      <GlobalStyle />
+      <H1>The Wild Oasis </H1>
+      <div>
+        <Button>Check in</Button>
+        <Button>Check out</Button>
+      </div>
+      <div>
+        <Input placeholder="inter your name" />
+      </div>
+    </StyledApp>
+  );
+}
 export default App;
