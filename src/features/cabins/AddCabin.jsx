e4253f4121b05348c6f9179fc64c2835b0c4cce0
sharif <sharif@ajab.sharifulislam.com>
2024-01-09 12:09:48 +0300

    clean cabins page file

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import Modal from "../../ui/Modal";
+import CreateCabinForm from "./CreateCabinForm";
+import Button from "../../ui/Button";
+
 function AddCabin() {
-  return <div>AddCabin</div>;
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setIsOpenModal((show) => !show)}>
+        {!isOpenModal ? "add new" : "Close"}
+      </Button>
+      {isOpenModal && (
+        <Modal onClose={() => setIsOpenModal(false)}>
+          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+        </Modal>
+      )}
+    </>
+  );
 }
 
 export default AddCabin;
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,15 +1,9 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
-import Button from "../ui/Button";
-import { useState } from "react";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
-import Modal from "./../ui/Modal";
-
-// import { getCabins } from "./../services/apiCabins";
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -19,14 +13,7 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
-        <Button onClick={() => setShowForm((show) => !show)}>
-          {!showForm ? "add new" : "Close"}
-        </Button>
-        {showForm && (
-          <Modal onClose={() => setShowForm(false)}>
-            <CreateCabinForm onCloseModal={() => setShowForm(false)} />
-          </Modal>
-        )}
+        <AddCabin />
       </Row>
     </>
   );
